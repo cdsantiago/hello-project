75535daf6938b761f7d08edf274ecbc383541cdf
Carlos Santiago <carlosdanielsantiagom@gmail.com>
2022-10-25 00:46:20 -0400

    Add logic to the 'Say hello' button, minor changes to the html

--- scripts/hello.js
+++ scripts/hello.js
@@ -1,1 +1,17 @@
-console.log("Hello there, we are glad you are here :)");
+//select the button and assign it to a variable
+const sayHelloBtn = document.querySelector(".say-hello-button");
+
+//create a function to pass it later to the event listener
+const makeFigureShake = () => {
+    //select the image and assign it
+    const helloImage = document.querySelector(".main-landing-image");
+    
+    //add a class to it that plays the animation
+    helloImage.classList.remove("animate__animated", "animate__shakeX",) //remove the class
+    void helloImage.offsetWidth // trigger reflow/refresh the DOM (I need to learn what "reflow" is and why this works
+    // https://developers.google.com/speed/docs/insights/browser-reflow)
+    helloImage.classList.add("animate__animated", "animate__shakeX",) //add the class after it was removed earlier
+    
+}
+//add the event listener to the button , trigger the function on the click event
+sayHelloBtn.addEventListener("click", makeFigureShake )
